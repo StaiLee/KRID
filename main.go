da89ba3472b0b58291bda65a15bf001e81267b32
StaiLee <ilyesstaili@gmail.com>
2023-04-28 15:27:00 +0200

    setup server

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
-const port = 1706
+type Page struct {
+	Valeur string
+}
+
+const port = 1709
+
+var Templateshome = template.Must(template.ParseFiles("./static/templates/index.html"))
 
 func main() {
-	fmt.Println("test")
+	http.HandleFunc("./index", homeHandler)
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	fmt.Println("http://localhost:1709 - server started on port", port)
+	http.ListenAndServe(":1709", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	p := Page{Valeur: "Home page"}
+	err := Templateshome.ExecuteTemplate(w, "index.html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
