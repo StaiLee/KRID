d781e37280c762e8c45a4fa0753629a5f1f79549
StaiLee <102300908+StaiLee@users.noreply.github.com>
2023-05-02 22:52:38 +0200

    server finis

--- go/server.go
+++ go/server.go
@@ -17,10 +17,11 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func Start() {
-	http.Handle("/assets/css/", http.StripPrefix("/assets/css/", http.FileServer(http.Dir("./static/assets/css/"))))
-	http.Handle("/assets/js/", http.StripPrefix("/assets/js/", http.FileServer(http.Dir("./static/assets/js/"))))
-	http.Handle("/assets/images/", http.StripPrefix("/assets/images/", http.FileServer(http.Dir("./static/assets/images/"))))
-	http.HandleFunc("/index.html", homeHandler)
-	fmt.Printf("Started server successfully on http://localhost:1709/home\n")
+	http.Handle("/static/assets/style/", http.StripPrefix("/static/assets/style/", http.FileServer(http.Dir("./static/assets/style/"))))
+	http.Handle("/static/assets/js/", http.StripPrefix("/static/assets/js/", http.FileServer(http.Dir("./static/assets/js/"))))
+	http.Handle("/static/assets/images/", http.StripPrefix("/static/assets/images/", http.FileServer(http.Dir("./static/assets/images/"))))
+	http.Handle("/static/templates/", http.StripPrefix("/static/templates/", http.FileServer(http.Dir("./static/templates"))))
+	http.HandleFunc("/", homeHandler)
+	fmt.Printf("Started server successfully on http://localhost:1709\n")
 	http.ListenAndServe(":1709", nil)
 }
