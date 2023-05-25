20839adadb167ccfda5745a8d2ed4440f61d7090
StaiLee <102300908+StaiLee@users.noreply.github.com>
2023-05-26 00:35:07 +0200

    setup db

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module module
 
 go 1.17
+
+require github.com/mattn/go-sqlite3 v1.14.16 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import Server "module/go"
+import (
+	"database/sql"
+	"log"
+	Server "module/go"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
 	Server.Start()
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        email TEXT,
+		password TEXT
+    );
+`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(`
+    INSERT INTO users (name, email) VALUES (?, ?);
+`, "John Doe", "john@example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
