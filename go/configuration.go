a5341d1894982656356e6782505db218421c9b8d
StaiLee <102300908+StaiLee@users.noreply.github.com>
2023-06-06 00:03:58 +0200

    vive le go YOUPIIII

--- go/configuration.go
+++ go/configuration.go
@@ -1,33 +1,5 @@
 package module
 
-//FRONT
-//page about us, petite présentation + (nos git,linkedin,mails)
-//profile is looking rather bad
-//ajout report,like et le mettre dans la boite contenant le post
-//notif dans le header a gauche de about us j'arrive pas à le placer and place on top of buttons
-//bouton ajout de post + joli
-//admin panel moderation (report handling, mod list promote or demote, communities management)
-//slashs a gerer affichage
-//page activité/profil de l'utilisateur (created post, commentaires(show post and comment), post liked or disliked)
-//notif dans le header a gauche de about us like dislike post
-
-//BACKEND
-//admin functions missing
-//encrypt database
-//authentification avec google and github NEED IP DOMAIN
-//implementer https
-//docker
-//modele entitées relation
-//powerpoint
-
-//communities
-//scroll infini 10 par 10 posts
-//user profile picture
-//ajout d'emojis
-//password reset
-//email verification
-//compteurs de messages utilisateur
-
 import "os"
 
 var post Publication   //posts data to print
